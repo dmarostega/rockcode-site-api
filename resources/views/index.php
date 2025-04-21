f1322bdf88b26941918b2247b50da87bb3d1661f
dmarostega@gmail.com <dmarostega@gmail.com>
2025-04-21 16:07:23 -0300

    feat(sistema): implementado index padrão.

---
+++ resources/views/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rock code Labs (blade)</title>
+    <style>
+
+        *, *::before, *::after {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: #363636;
+            color: white;            
+        }
+
+        .container {
+            background-color: black;
+            display:flex;
+            flex-direction: column;
+            justify-content: center;    
+            width: 80vw;
+            height: 100vh; 
+            margin: 0 auto;
+            text-align: center;
+        }
+
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h1>Bem vindo a Rock Code Labs</h1>
+            <p>Site em Construção com Vue.js</p>
+        </div>
+    </div>
+</body>
+</html>
